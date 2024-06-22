1abd67df2f2cc8c19de2325fb90123a221b73de7
Alejandro López Tovar <alejandroloto@gmail.com>
2024-06-22 14:53:33 +0200

    Inicio y destruyo sesión

--- Sesiones/logout.php
+++ Sesiones/logout.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
+session_start(); // Inicia o reanuda la sesión
+session_unset(); // Limpiar todas las variables de sesión. Elimina los valores almacenados en la sesión actual sin destruir la sesión en sí.
+session_destroy(); // Destruir la sesión
 
 // Redirigimos al formulario de inicio de sesión
 header("Location: index.html");
